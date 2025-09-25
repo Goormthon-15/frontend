c1f11df23c5603f1617f3f04eb0a6a9655f91662
zeriong <jaeryong95@gmail.com>
2025-09-25 14:58:22 +0900

    feat: mobile frame 추가가

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import { VStack } from "@vapor-ui/core";
+import { HStack, VStack } from "@vapor-ui/core";
 import { DashboardHeader } from "./_components/DashboardHeader";
 
 export default function DashboardLayout({
@@ -7,9 +7,19 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <VStack>
-      <DashboardHeader />
-      {children}
-    </VStack>
+    <HStack
+      width={"100%"}
+      height={"100vh"}
+      justifyContent="center"
+      alignItems="center"
+      backgroundColor={"$gray-050"}
+    >
+      <VStack width={"400px"} height={"720px"} backgroundColor={"#ffffff"}>
+        <DashboardHeader />
+
+        {/*  Content  */}
+        <div className={"grow w-full h-full"}>{children}</div>
+      </VStack>
+    </HStack>
   );
 }
